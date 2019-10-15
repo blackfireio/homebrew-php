64c9970e1623ea7ec3ea325cb99b43341e474f05
Marc Weistroff <marc@weistroff.net>
2019-10-15 02:44:37 +0200

    bump revision

--- Formula/php74.rb
+++ Formula/php74.rb
@@ -3,7 +3,7 @@ require File.expand_path("../../Abstract/abstract-php", __FILE__)
 class Php74 < AbstractPhp
   init
   desc "PHP Version 7.4"
-  revision 17
+  revision 18
 
   include AbstractPhpVersion::Php74Defs
 
